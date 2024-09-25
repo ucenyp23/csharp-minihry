cfacc6f4eb94662d45fcf546c9eff9e4afaa2d99
ucenyp23 <ucenyp23@spstrutnov.cz>
2024-09-25 13:02:48 +0200

    Add project files.

---
+++ kamen-nuzky-papir/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace kamen_nuzky_papir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
